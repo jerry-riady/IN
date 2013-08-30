20b98b6efceaa87b23ffb442775fd63212546ee8
jerry-riady <jerry-riady@users.noreply.github.com>
2013-08-31 04:01:29 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+
+// AdSense Setting
+$adOn        = "0";                           // Turn AdSense ads on? 1 = on, 0 = off
+$publisherID = "";                            // Your numeric publisher ID - e.g. XXXXXXXXXXXXXXXX (without the pub-)
+$channelID   = "";                            // Your adsense channel ID - leave as blank, i.e. "" if not used.
+
+// Site Information
+$siteURL = "http://www.sitetoolcenter.com";   // The URL of your site
+
+?>
